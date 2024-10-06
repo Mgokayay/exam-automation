a730a5240fe0678a7949a35e18e6373eed69d6d1
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-10-06 15:46:20 +0300

    Question select CSS completed

---
+++ src/components/QuestionSelect.tsx
@@ -0,0 +1,42 @@
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../store/store";
+import "../styles/questionSelect.scss";
+import { selectQuestion } from "../store/slices/questionSlice";
+
+export default function QuestionSelect() {
+  const dispatch = useDispatch<AppDispatch>();
+
+  const questions = useSelector((state: RootState) => state.question.questions);
+  console.log("Sorular: ", questions);
+
+  const optionLabels = ["A", "B", "C", "D", "E"];
+
+  return (
+    <div className="questionSelect-main">
+      <div className="header">
+        <img src="/icons/questionSelect/ders.svg" alt="Ders Icon" />
+        <div>
+          <p>Türkçe</p>
+          <p>{questions.length} Soru</p>
+        </div>
+      </div>
+      {questions.map((question) => (
+        <button
+          key={question.id}
+          onClick={() => dispatch(selectQuestion(question.id))}
+          className="questions"
+        >
+          <div className="questionOrder">{question.id}.Soru</div>
+
+          <div className="options">
+            {question.options.map((option, index) => (
+              <div key={option.id} className="option">
+                <p>{optionLabels[index]}</p>
+              </div>
+            ))}
+          </div>
+        </button>
+      ))}
+    </div>
+  );
+}
