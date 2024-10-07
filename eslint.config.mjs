1dbae8333df7e92288ab11f7687b68806265f7e9
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-10-07 18:31:14 +0300

    Eslint config edited

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,16 +4,36 @@ import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: globals.browser } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+        settings: {
+          react: {
+            version: "detect",
+          },
+        },
+      },
+    },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {
+    ...pluginReact.configs.flat.recommended,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
   {
     rules: {
-      "no-console": "warn", // Konsol uyarısı
-      eqeqeq: "error", // Eşitlik kontrolü hatası
-      "react/prop-types": "off", // PropTypes kullanımı zorunlu değil
+      "no-console": "warn",
+      eqeqeq: "error",
+      "react/prop-types": "off",
       "react/react-in-jsx-scope": "off",
     },
   },
