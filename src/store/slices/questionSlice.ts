236bc2d6d1c601ee3b1cc4ede2bd10d3587874cd
Mgokayay <140398516+Mgokayay@users.noreply.github.com>
2024-10-07 18:02:36 +0300

    Reducers added

--- src/store/slices/questionSlice.ts
+++ src/store/slices/questionSlice.ts
@@ -15,20 +15,66 @@ interface Question {
 
 interface QuestionsState {
   questions: Question[];
+  selectedQuestion: Question | null;
+  selectedOption: { [key: number]: number | null };
+  showAnswers: boolean;
 }
 
 const initialState: QuestionsState = {
   questions: questionsData,
+  selectedQuestion: null,
+  selectedOption: {},
+  showAnswers: false,
 };
 
 export const questionSlice = createSlice({
   name: "question",
   initialState,
-  reducers: {},
+  reducers: {
+    selectQuestion: (state, action: PayloadAction<number>) => {
+      state.selectedQuestion =
+        state.questions.find((q) => q.id === action.payload) || null;
+    },
+    selectOption: (
+      state,
+      action: PayloadAction<{ questionId: number; optionId: number }>
+    ) => {
+      state.selectedOption[action.payload.questionId] = action.payload.optionId;
+    },
+    goToNextQuestion: (state) => {
+      if (state.selectedQuestion) {
+        const currentIndex = state.questions.findIndex(
+          (q) => q.id === state.selectedQuestion?.id
+        );
+        const nextQuestion = state.questions[currentIndex + 1];
+        if (nextQuestion) {
+          state.selectedQuestion = nextQuestion;
+        }
+      }
+    },
+    goToPreviousQuestion: (state) => {
+      if (state.selectedQuestion) {
+        const currentIndex = state.questions.findIndex(
+          (q) => q.id === state.selectedQuestion?.id
+        );
+        const previousQuestion = state.questions[currentIndex - 1];
+        if (previousQuestion) {
+          state.selectedQuestion = previousQuestion;
+        }
+      }
+    },
+    toggleShowAnswers: (state) => {
+      state.showAnswers = !state.showAnswers;
+    },
+  },
 });
 
 export const {
-  /** reducers */
+  selectQuestion,
+  selectOption,
+  goToNextQuestion,
+  goToPreviousQuestion,
+  toggleShowAnswers,
 } = questionSlice.actions;
 
 export default questionSlice.reducer;
